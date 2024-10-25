290f3327931afb364270105f2f754431c8023d70
Sothyra12 <schan256@my.centennialcollege.ca>
2024-10-25 00:57:58 -0400

    looped through the elements and created a reset() function

--- script.js
+++ script.js
@@ -18,6 +18,15 @@ const taskData = [];
 // track the state when editing & discarding tasks
 let currentTask = {};
 
+// reset the form inputs after adding a task
+const reset = () => {
+  titleInput.value = "";
+  dateInput.value = "";
+  descriptionInput.value = "";
+  taskForm.classList.toggle("hidden");
+  currentTask = {};
+};
+
 // open and close the form modal using classList.toggle instead of classlist.add/.remove
 openTaskFormBtn.addEventListener("click", () =>
   taskForm.classList.toggle("hidden")
@@ -31,7 +40,7 @@ cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
 
 discardBtn.addEventListener("click", () => {
   confirmCloseDialog.close();
-  taskForm.classList.toggle("hidden");
+  reset();
 });
 
 // save the input values to taskData array and render the task on the page
@@ -45,4 +54,25 @@ taskForm.addEventListener("submit", (e) => {
     description: descriptionInput.value,
   };
 
+  // if the task is being edited, update the taskData array with the new task object
+  if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  }
+
+  // loop through the taskData array and render each task on the page
+  taskData.forEach(({id, title, date, description}) => {
+      tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+          <p><strong>Title:</strong> ${title}</p>
+          <p><strong>Date:</strong> ${date}</p>
+          <p><strong>Description:</strong> ${description}</p>
+          <button type="button" class="btn">Edit</button>
+          <button type="button" class="btn">Delete</button>
+        </div>
+      `
+    }
+  );
+
+  // reset the form inputs after adding a task
+  reset();
 });
